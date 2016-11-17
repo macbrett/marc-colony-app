110c40a032e366dd4112ece5cd3c2a7d166efa03
Brett MacDonald <macdonald.brett@outlook.com>
2016-11-16 18:37:28 -0800

    added aliens component

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Encounter } from '../models';
+import { Alien } from '../models';
+import AliensService from '../services/aliens.service';
+import {FormGroup, FormControl, FormBuilder, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
-  styleUrls: ['./report.component.css']
+  styleUrls: ['./report.component.css'],
+  providers: [AliensService]
 })
 export class ReportComponent implements OnInit {
 
-  constructor() { }
+  marsAliens:Alien[];
+  registerForm: FormGroup;
+
+
+
+  constructor(aliensService: AliensService) {
+
+ aliensService.getAliens().subscribe((aliens) => {
+  this.marsAliens = aliens;
+  });
+
+}
 
   ngOnInit() {
   }
+
 
 }
