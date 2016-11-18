b0e5c160717d1506664861af34570edf621c072a
Brett MacDonald <macdonald.brett@outlook.com>
2016-11-18 15:32:14 -0800

    finished slider

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,17 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding,
+         trigger, transition, animate,
+         style, state  } from '@angular/core';
 
 
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss'],
+  animations: [
+    trigger('routeAnimation', [
+      state('*',
+        style({
+          opacity: 1,
+          transform: 'translateX(0)'
+        })
+      ),
+      transition(':enter', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100%)'
+        }),
+        animate('0.2s ease-in')
+      ]),
+      transition(':leave', [
+        animate('0.5s ease-out', style({
+          opacity: 0,
+          transform: 'translateY(100%)'
+        }))
+      ])
+    ])
+  ]
 })
 export class WelcomeComponent implements OnInit {
-
+@HostBinding('@routeAnimation') get routeAnimation() {
+    return true;}
+      @HostBinding('style.display') get display() {
+    return 'block';
+  }
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
